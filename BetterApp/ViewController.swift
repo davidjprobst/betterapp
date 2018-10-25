29efde796bfe47fea784fe78a29ca8bcedd611cc
David Probst <probst.dj2@gmail.com>
2018-10-25 12:35:18 -0500

    Initial Commit

---
+++ BetterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BetterApp
+//
+//  Created by David Probst on 10/25/18.
+//  Copyright © 2018 David Probst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
